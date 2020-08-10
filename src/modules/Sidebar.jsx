352723821f9326ac8ca9f13fd2d3f09d3c943de3
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-08-10 13:25:38 -0700

    get request with specific entery but can't getting child contents

--- src/modules/Sidebar.jsx
+++ src/modules/Sidebar.jsx
@@ -1,22 +1,45 @@
-import React, { Fragment, Component} from 'react';
-import { Client } from './../Client';
-
+import React, { Fragment, Component } from "react";
+import { Client } from "./../Client";
 
 export default class Sidebar extends Component {
-    
-    componentDidMount() {
-          Client.getEntries()
-          .then((entries) => {
-              console.log(entries.items[1])
-          })
-          .catch(console.error)
-          
-        }
-    render() {
-        return (
-            <Fragment>
-            <h1>Sidebar</h1>
-            </Fragment>
-        );
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			section: "",
+		};
+	}
+
+	componentDidMount() {
+		Client.getEntry("3VhrFwooIpqpHDuZTZ2axF")
+			.then((response) => {
+				const data = response.fields;
+				console.log(data);
+
+				this.setState({
+					section: data,
+				});
+			})
+
+			.catch(console.error);
+		
+	}
+
+	render() {
+		const view = this.state.section;
+		// console.log(view);
+		const { heading, testin2, menuheadings } = view;
+		return (
+			<Fragment>
+				<section className="sidebar">
+					<div className="sidebar__block">
+						<div className="sidebar__contents">
+							<h1>{heading}</h1>
+							<h3>{testin2}</h3>
+							<h2>{}</h2>
+						</div>
+					</div>
+				</section>
+			</Fragment>
+		);
+	}
 }
