b30ea173c86e41c35c6fb7950bd106ffcd68ebe0
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-08-10 14:49:13 -0700

    can't able to fetch nested refernce contents

--- src/modules/Sidebar.jsx
+++ src/modules/Sidebar.jsx
@@ -1,45 +1,55 @@
-import React, { Fragment, Component } from "react";
-import { Client } from "./../Client";
+import React, { Fragment, Component} from 'react';
+import { Client } from './../Client';
+
 
 export default class Sidebar extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			section: "",
-		};
-	}
 
-	componentDidMount() {
-		Client.getEntry("3VhrFwooIpqpHDuZTZ2axF")
-			.then((response) => {
-				const data = response.fields;
-				console.log(data);
+    constructor (props) {
+        super(props);
+        this.state = {
+            section: ""
+        }
+    }
+    
+    componentDidMount() {
 
-				this.setState({
-					section: data,
-				});
-			})
+        
 
-			.catch(console.error);
-		
-	}
+        Client.getEntries({
+            include: 3
+          })
+          .then((response) => {
+              const data = response.items[0].fields;
+            //   console.log(data)
+              this.setState ({
+                  section: data
+              })
+              console.log(this.state.section);
+          })
+          .catch(console.error)
 
-	render() {
-		const view = this.state.section;
-		// console.log(view);
-		const { heading, testin2, menuheadings } = view;
-		return (
-			<Fragment>
-				<section className="sidebar">
-					<div className="sidebar__block">
-						<div className="sidebar__contents">
-							<h1>{heading}</h1>
-							<h3>{testin2}</h3>
-							<h2>{}</h2>
-						</div>
-					</div>
-				</section>
-			</Fragment>
-		);
-	}
+          
+          
+        }
+       
+    render() {
+
+        
+        const view = this.state.section;
+        const { heading, testin2, menuheadings} = view;
+
+        return (
+            <Fragment>
+          <section className = "sidebar">
+          <div className = "sidebar__block">
+          <div className = "sidebar__contents">
+           <h1>{heading}</h1>
+           <h3>{testin2}</h3>
+         <h2>{menuheadings.fields.about}</h2>
+          </div>
+          </div>
+          </section>
+            </Fragment>
+        );
+    }
 }
