7aa33019ea4f9284e5f5464adad6047a3e2512c7
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-08-10 07:51:55 -0700

    api is hidden

--- src/modules/Sidebar.jsx
+++ src/modules/Sidebar.jsx
@@ -1,15 +1,11 @@
 import React, { Fragment, Component} from 'react';
-var contentful = require('contentful');
+import { Client } from './../Client';
+
 
 export default class Sidebar extends Component {
     
     componentDidMount() {
-        var client = contentful.createClient({
-            space: 'z5brs5mrqys2',
-            accessToken: 'gra_-04l_O8Iueoa3VgVjCUN5cA-E8v36K9iNCdFuSQ'
-          })
-
-          client.getEntries()
+          Client.getEntries()
           .then((entries) => {
               console.log(entries.items[1])
           })
