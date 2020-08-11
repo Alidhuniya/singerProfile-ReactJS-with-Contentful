0a56cd52e357f6569d01fda5ffd5513760cd7215
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-08-11 16:19:50 -0700

    about compoents added

--- src/modules/About.jsx
+++ src/modules/About.jsx
@@ -37,11 +37,28 @@ class About extends Component {
         const view = this.state.section;
         
         // console.log(view);
-        const {subHeading} = view;
+        const {subHeading, title, subtitle, hiremeBtn} = view;
         return (
 
             <Fragment>
-            <h1>{subHeading}</h1>
+            <section className = "about">
+            <div className = "about__block">
+            <p className = "about__subHeading">{subHeading}</p>
+            <div className = "about__headingContainer">
+            <h1 className = "about__heading">{title}</h1>
+            <div className ="about__socialIcons">
+            <i className="fab fa-facebook socialIcon"></i>
+            <i className="fab fa-instagram-square socialIcon"></i>
+            <i className="fab fa-twitter socialIcon"></i>
+            </div>
+            </div>
+            <p className = "about__subtitle">{subtitle}</p>
+            <p className = "about__p0">{view && view.description.content[0].content[0].value}</p>
+            <p className = "about__p1">{view && view.description.content[1].content[0].value}</p>
+            <p className = "about__p2">{view && view.description.content[2].content[0].value}</p>
+            <button className = "about__btn">{hiremeBtn}</button>
+            </div>
+            </section>
             </Fragment>
 
         );
