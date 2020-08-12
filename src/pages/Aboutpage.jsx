4ad304c57541d2bc36ee7a1f98b4fd21ad39f6d3
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-08-11 22:34:20 -0700

    home page component resused in about page

--- src/pages/Aboutpage.jsx
+++ src/pages/Aboutpage.jsx
@@ -1,12 +1,26 @@
 import React, { Fragment, Component} from 'react';
 import "../sass/4-pages/about/_about.scss";
+import "../sass/4-pages/home/_home.scss";
+import Sidebar from './../modules/Sidebar';
+import About from './../modules/About';
+import Upcoming from './../modules/Upcoming';
+import Footer from './../modules/Footer';
 
 class Aboutpage extends Component {
     render() {
         return (
             <Fragment>
-            <section className = "about" >
-            <h1>ABout Page</h1>
+            <section className = "home" >
+            <div className = "home__leftContainer">
+            <Sidebar />
+            </div>
+            <div className = "home__rightContainer">
+            <About />
+            <Upcoming className = "home__upcoming" />
+            <div className = "home__footer">
+            <Footer />
+            </div>
+            </div>
             </section>
             </Fragment>
             
