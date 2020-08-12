f0d571c05c1f16facdb0a941ba06d3007856bea1
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-08-11 18:27:41 -0700

    home page added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Fragment, Component } from "react";
-// import Home from './pages/Home';
-// import About from "./modules/About";
-// import Upcoming from './modules/Upcoming';
-import Footer from './modules/Footer';
+import Home from './pages/Home';
 
 class App extends Component {
 	render() {
 		return (
 			<Fragment>
-				{/* <Home /> */}
-          {/*<About /> */}
-          {/*<Upcoming /> */}
-          <Footer />
+			<Home />
 			</Fragment>
 		);
 	}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,26 @@
 import React, { Fragment, Component} from 'react';
 import Sidebar from './../modules/Sidebar';
+import About from './../modules/About';
+import Upcoming from './../modules/Upcoming';
+import Footer from './../modules/Footer';
+import "../sass/4-pages/home/_home.scss";
 
 class Home extends Component {
     render() {
         return (
             <Fragment>
+            <section className = "home" >
+            <div className = "home__leftContainer">
             <Sidebar />
+            </div>
+            <div className = "home__rightContainer">
+            <About />
+            <Upcoming className = "home__upcoming" />
+            <div className = "home__footer">
+            <Footer />
+            </div>
+            </div>
+            </section>
             </Fragment>
             
         );
